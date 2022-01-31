f717c2511d9a3f568dc8eb0621e1277d048a72ff
LeonovS <leon19840709@gmail.com>
2022-01-31 22:02:40 +0300

    реактивная форма

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,16 +7,9 @@ import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FormComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
+  declarations: [AppComponent, FormComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,61 +1,76 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm, ValidationErrors } from '@angular/forms';
-import { User } from '../user';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NgControl } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss'],
 })
-export class FormComponent implements OnInit, AfterViewInit {
-  user: User = new User(1, null, null, null);
-  roles: string[] = ['Гость', 'Модератор', 'Администратор'];
-
-  formErrors: any = {
-    name: '',
-    age: '',
-  };
-
-  validetionMessages: any = {
-    name: {
-      required: 'Имя обязательно',
-      minlength: 'Имя должно содержать минимум 4 символа',
-    },
-
-    age: {
-      required: 'Возраст обязателен',
-    },
-  };
-
-  @ViewChild('userForm') userForm!: NgForm;
+export class FormComponent implements OnInit {
+  userForm = new FormGroup({
+    name: new FormControl('user name'),
+    password: new FormControl('3126465'),
+  });
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  ngAfterViewInit(): void {
-    this.userForm.valueChanges?.subscribe(() => this.onValueChanged());
-  }
-
-  onValueChanged(): void {
-    const form = this.userForm.form;
-
-    Object.keys(this.formErrors).forEach((field) => {
-      this.formErrors[field] = '';
-
-      const control = form.get(field);
-
-      if (control && control.dirty && control.invalid) {
-        const messages = this.validetionMessages[field];
-
-        Object.keys(control.errors as ValidationErrors).forEach((key) => {
-          this.formErrors[field] = messages[key];
-        });
-      }
-    });
-  }
-
   onSubmit(): void {
-    console.log('form submitted');
+    console.log(this.userForm.invalid);
+    console.log(this.userForm.value);
   }
 }
+
+// export class FormComponent implements OnInit, AfterViewInit {
+//   user: User = new User(1, null, null, null);
+//   roles: string[] = ['Гость', 'Модератор', 'Администратор'];
+
+//   formErrors: any = {
+//     name: '',
+//     age: '',
+//   };
+
+//   validetionMessages: any = {
+//     name: {
+//       required: 'Имя обязательно',
+//       minlength: 'Имя должно содержать минимум 4 символа',
+//     },
+
+//     age: {
+//       required: 'Возраст обязателен',
+//     },
+//   };
+
+//   @ViewChild('userForm') userForm!: NgForm;
+
+//   constructor() {}
+
+//   ngOnInit(): void {}
+
+//   ngAfterViewInit(): void {
+//     this.userForm.valueChanges?.subscribe(() => this.onValueChanged());
+//   }
+
+//   onValueChanged(): void {
+//     const form = this.userForm.form;
+
+//     Object.keys(this.formErrors).forEach((field) => {
+//       this.formErrors[field] = '';
+
+//       const control = form.get(field);
+
+//       if (control && control.dirty && control.invalid) {
+//         const messages = this.validetionMessages[field];
+
+//         Object.keys(control.errors as ValidationErrors).forEach((key) => {
+//           this.formErrors[field] = messages[key];
+//         });
+//       }
+//     });
+//   }
+
+//   onSubmit(): void {
+//     console.log('form submitted');
+//   }
+// }
